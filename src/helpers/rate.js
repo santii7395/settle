52156e0ec45f810a2f3c400c702d64a76a280240
Santiago Carabone <santiagocarabone@MacBook-Pro-de-Santiago.local>
2022-02-26 00:30:58 -0300

    host change

--- src/helpers/rate.js
+++ src/helpers/rate.js
@@ -1,7 +1,7 @@
 module.exports = function Rate(pair, rate, fee){
     this.Pair = pair,
     this.Original_Rate= rate,
-    this.Fee_%= fee,
+    this.Fee= fee,
     this.Fee_amount= rate * (fee / 100),
     this.Rate_with_markup_fee = rate * (fee / 100 + 1)
 }
