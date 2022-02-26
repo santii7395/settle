6a1546bde99c62d8abb35aa2e9b0f1ea25c30224
Santiago Carabone <santiagocarabone@MacBook-Pro-de-Santiago.local>
2022-02-26 00:25:59 -0300

    host change

--- src/lib/hapi.js
+++ src/lib/hapi.js
@@ -3,7 +3,7 @@ const routes = require('../routes/routes');
 
 const server = Hapi.server({
   port: 3000,
-  host: 'localhost',
+  host: 'http://ec2-54-197-207-63.compute-1.amazonaws.com',
   app: {}
 });
 
