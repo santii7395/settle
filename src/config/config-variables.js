58d1f90ea721594a5241c8e3f9f8fb3c3ee82bcb
Santiago Carabone <santiagocarabone@MacBook-Pro-de-Santiago.local>
2022-02-27 16:05:31 -0300

    fix .env

--- src/config/config-variables.js
+++ src/config/config-variables.js
@@ -4,6 +4,6 @@ require('dotenv').config({path: __dirname+'/./../../.env'});
 module.exports = {
     apiURL: process.env.api_url,
     apiKey: process.env.api_key,
-    host: process.host,
-    port: process.port
+    host: process.env.host,
+    port: process.env.port
 }
